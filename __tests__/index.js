7f6be1578a3cfec69239a0d847966065f3bd5586
twinh <twinhuang@qq.com>
2021-05-09 23:51:53 +0800

    test(event): 初始化测试

---
+++ __tests__/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import {Event} from '..';
+
+describe('test', () => {
+  test('basic', () => {
+    const result = render(<Event/>);
+
+    expect(result.container).toMatchSnapshot();
+  });
+});
